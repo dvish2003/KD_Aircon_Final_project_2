892f8bb9ea76d5d58de77bbc5642f4856f5aed6d
vishmee <Vishanchathuranga2003@gmail.com>
2024-07-07 23:09:09 +0530

    Commit

--- src/main/java/lk/Ijse/BO/OrderBO/OrderDetailBO.java
+++ src/main/java/lk/Ijse/BO/OrderBO/OrderDetailBO.java
@@ -8,6 +8,6 @@ import java.util.List;
 public interface OrderDetailBO {
           public  boolean save(List<OrderDetail> odList) throws SQLException, ClassNotFoundException;
 
-//
+
         public   boolean saveOrderDetail(OrderDetail od) throws SQLException, ClassNotFoundException;
 }
